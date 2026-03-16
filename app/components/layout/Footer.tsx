e8b95fa69f8a8f699346ecc1af24b8725c224971
Semir Reyhan <semir.reyhan1990@gmail.com>
2026-03-16 19:44:30 +0300

    feat: initial commit

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>
+    <div>
+        </div>Footer</div>;
+};
+
+export default Footer;
---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>HeaderPage</div>;
+};
+
+export default Header;
